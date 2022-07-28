e8ba1ace944b3994a003dc2a72ab0ef4de5e1368
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-07-28 12:02:14 -0400

    updated highscores in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -159,7 +159,7 @@ function displayEndScreen() {
   var scoresText = "";
   for (let i = 0; i < scores.length; i++) {
     const score = scores[i];
-    scoresText += "Highscores " + "\n" + "name: " + score.Initials + ", score: " + score.score + "\n";
+    scoresText += "Highscores: " + "initials " + score.Initials + " and score " + score.score + "; " + "\n";
   }
   finalScoreEl.textContent = scoresText;
   document.querySelector(".timer-text").innerHTML = "Your score is: " + timerCount
